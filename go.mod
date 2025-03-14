81f7031dbfea08f54429d364dd5b7611f5417fbd
Sam Tai <shunwatai@gmail.com>
2025-03-14 16:47:15 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/shunwatai/pedelf
+
+go 1.23.2
+
+require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/pdfcpu/pdfcpu v0.9.1
+)
+
+require (
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ pkg/pedelf/main.go
@@ -0,0 +1,102 @@
+package pedelf
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"strconv"
+
+	"github.com/disintegration/imaging"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+)
+
+type Context struct {
+	*model.Context
+	TotalPages int
+	Src        io.ReadSeeker
+	Images     []map[int]model.Image
+}
+
+func GetCtxFromInput(src io.ReadSeeker) (*Context, error) {
+	ctx, err := api.ReadContext(src, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to ReadContext, err: %s", err.Error())
+	}
+
+	pdfcpu.OptimizeXRefTable(ctx)
+	api.OptimizeContext(ctx)
+
+	ctx.EnsurePageCount()
+	pages := ctx.PageCount
+	PedelfCtx := &Context{
+		Context:    ctx,
+		TotalPages: pages,
+		Src:        src,
+	}
+
+	return PedelfCtx, nil
+}
+
+func (ctx *Context) SetRawImagesFromGivenPages() error {
+	log.Printf("total pages: %+v\n", ctx.TotalPages)
+
+	pagesStr := []string{}
+	for page := range ctx.TotalPages {
+		pagesStr = append(pagesStr, strconv.Itoa(page))
+	}
+
+	rawImages, err := api.ExtractImagesRaw(ctx.Src, pagesStr, nil)
+	if err != nil {
+		return fmt.Errorf("failed to ExtractImagesRaw, err: %+v", err.Error())
+	}
+
+	ctx.Images = rawImages
+
+	return nil
+}
+
+func (ctx *Context) CompressImages(compressLvl int) error {
+	if compressLvl <= 1 {
+		return fmt.Errorf("compressLvl must be larger than 1")
+	}
+	jpegQuality := 70
+
+	for _, rawImage := range ctx.Images {
+		for _, img := range rawImage {
+			log.Printf("img: name-%s, type-%s, page-%d, %dx%d\n", img.Name, img.FileType, img.PageNr, img.Width, img.Height)
+
+			im, err := imaging.Decode(img)
+			if err != nil {
+				// unsupported image format may happen here, skip handling it
+				fmt.Errorf("image.Decode err: %+v\n", err.Error())
+				continue
+			}
+
+			resizedImg := imaging.Fit(im, im.Bounds().Dx()/compressLvl, im.Bounds().Dy()/compressLvl, imaging.Lanczos)
+
+			smallerBuf := new(bytes.Buffer)
+			err = imaging.Encode(smallerBuf, resizedImg, imaging.JPEG, imaging.JPEGQuality(jpegQuality))
+			if err != nil {
+				return fmt.Errorf("jpeg.Encode err: %+v", err.Error())
+			}
+
+			sd2, _, _, _ := model.CreateImageStreamDict(ctx.XRefTable, smallerBuf, false, false)
+			ctx.XRefTable.Table[img.ObjNr].Object = *sd2
+		}
+	}
+
+	return nil
+}
+
+func (ctx *Context) GetPdfBuffer() (*bytes.Buffer, error) {
+	ctx.EnsureVersionForWriting()
+	wr := new(bytes.Buffer)
+	if err := api.WriteContext(ctx.Context, wr); err != nil {
+		return nil, fmt.Errorf("failed to WriteContext, err: %s", err.Error())
+	}
+
+	return wr, nil
+}
